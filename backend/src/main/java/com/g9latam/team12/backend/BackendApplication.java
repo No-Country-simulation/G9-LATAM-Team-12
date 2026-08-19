152bc3962ca1eeff61cfb504fc2754fb7df3a19e
Leo <accruz@fi.uba.ar>
2026-08-19 18:46:09 +0000

    Migracion a MySQL_local

--- backend/src/main/java/com/g9latam/team12/backend/BackendApplication.java
+++ backend/src/main/java/com/g9latam/team12/backend/BackendApplication.java
@@ -1,13 +1,29 @@
 package com.g9latam.team12.backend;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+	@Bean
+	public Flyway flyway(DataSource dataSource) {
+		Flyway flyway = Flyway.configure()
+				.dataSource(dataSource)
+				.locations("classpath:db/migration")
+				.baselineOnMigrate(true)
+				.load();
+
+		flyway.migrate();
+
+		return flyway;
 	}
 
 }
--- backend/src/main/java/com/g9latam/team12/backend/service/ModeloPredictor.java
+++ backend/src/main/java/com/g9latam/team12/backend/service/ModeloPredictor.java
@@ -2,6 +2,7 @@ package com.g9latam.team12.backend.service;
 
 import com.g9latam.team12.backend.dto.AnalisisResponseDTO;
 import com.g9latam.team12.backend.dto.ConsumoRequestDTO;
+import org.springframework.stereotype.Service;
 
 public interface ModeloPredictor {
     AnalisisResponseDTO predecir(ConsumoRequestDTO request);
